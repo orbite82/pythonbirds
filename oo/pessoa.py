19f92af0618822de4dc8d0407bd7a29136e0076b
tnegraoo <thiagoorbite@gmail.com>
2019-06-12 15:07:36 -0300

    Criada a classe Pessoa 2

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,8 @@
 class Pessoa:
-    pass
+    def cuprimentar(self):
+        return f'Olá {id(self)}'
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cuprimentar(p))
+    print(id(p))
+    print(p.cuprimentar())
