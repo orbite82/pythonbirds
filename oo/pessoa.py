016b782238ba100405089af3ea4e2546b97255c1
tnegraoo <thiagoorbite@gmail.com>
2019-06-13 15:09:06 -0300

    Criados Atributos de Instancia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,38 @@
 class Pessoa:
+    def __init__(self, nome=None, idadde=35):
+        self.idadde = idadde
+        self.nome = nome
+
     def cuprimentar(self):
         return f'Olá {id(self)}'
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Luciene')
     print(Pessoa.cuprimentar(p))
     print(id(p))
     print(p.cuprimentar())
+    print(p.nome)
+    p.nome = 'Orbite'
+    print(p.nome)
+    print(p.idadde)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
