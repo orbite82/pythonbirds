cccaa4ac9e77273eafd58be918f35d6068d939bf
tnegraoo <thiagoorbite@gmail.com>
2019-07-04 15:23:11 -0300

    Cursos em Video Atualizando Desafios 1.0

--- Cursos em  Video/Aula09B.py
+++ Cursos em  Video/Aula09B.py
@@ -1,3 +1,5 @@
 frase = 'Curso em Vídeo Python'
 frase = frase.replace('Python', 'Android')
 print(frase)
+print(frase.split())
+print(frase.lower().find('vídeo'))
---
+++ Cursos em  Video/Aula09C.py
@@ -0,0 +1,5 @@
+frase = 'Curso em Vídeo Python'
+dividido = frase.split()
+print(dividido)
+print(dividido[0])
+print(dividido[2][3])
