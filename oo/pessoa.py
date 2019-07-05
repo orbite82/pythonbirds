4b3ee204818a285ba2bd9282274c7f5dbe564f6a
tnegraoo <thiagoorbite@gmail.com>
2019-07-05 11:16:28 -0300

    Criado atributo complexo filhos arquivo pessoas

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,19 +1,23 @@
 class Pessoa:
-    def __init__(self, nome=None, idadde=35):
-        self.idadde = idadde
+    def __init__(self, *filhos, nome=None, idade=35):
+        self.idadde = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cuprimentar(self):
         return f'Olá {id(self)}'
 if __name__ == '__main__':
-    p = Pessoa('Luciene')
-    print(Pessoa.cuprimentar(p))
-    print(id(p))
-    print(p.cuprimentar())
-    print(p.nome)
-    p.nome = 'Orbite'
-    print(p.nome)
-    print(p.idadde)
+    orbitex = Pessoa(nome = 'Orbitex')
+    luciano = Pessoa(orbitex, nome = 'Luciano')
+    print(Pessoa.cuprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cuprimentar())
+    print(luciano.nome)
+#    luciano.nome = 'Orbite'
+#    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
 
 
 
