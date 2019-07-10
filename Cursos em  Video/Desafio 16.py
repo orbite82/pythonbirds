71495c2c2b86f071ede199a9334039f5edca00fa
tnegraoo <thiagoorbite@gmail.com>
2019-07-10 15:12:58 -0300

    Conversor de temperaturas 1.2

--- Cursos em  Video/Desafio 16.py
+++ Cursos em  Video/Desafio 16.py
@@ -1,3 +1,5 @@
-import math
+#import math
+from  math import trunc
 num = float(input('Digite um valor: '))
-print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, math.trunc(num)))
+#print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, math.trunc(num)))
+print('O valor digitado foi {} e a sua porçõa inteira é {}'.format(num, trunc(num)))
