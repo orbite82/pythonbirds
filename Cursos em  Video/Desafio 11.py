e884b63ee41803dadd84bf5e33ddc4a4d5c6701e
tnegraoo <thiagoorbite@gmail.com>
2019-07-10 11:19:19 -0300

    Pintando Parede 1.0

---
+++ Cursos em  Video/Desafio 11.py
@@ -0,0 +1,4 @@
+larg = float(input('Largura da parede: '))
+altura = float(input('Altura da parede: '))
+area = larg * altura
+print('Sua parede tem a dimensão de {}x{} e sua área é de {}m²'.format(larg, altura, area))
