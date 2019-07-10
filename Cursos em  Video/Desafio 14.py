3e9e6845d297c056e28590ff9a737379d1695325
tnegraoo <thiagoorbite@gmail.com>
2019-07-10 14:42:01 -0300

    Conversor de temperaturas 1.1

--- Cursos em  Video/Desafio 14.py
+++ Cursos em  Video/Desafio 14.py
@@ -1,5 +1,6 @@
 c = float(input('Informe a temperatua em °C: '))
 f = ((9 * c) / 5) + 32
+#f = 9 * c / 5 + 32 correto tbm devido a ordem de precedencia
 print('A temperatura de {}°C corresponde a {}°F!'.format(c, f))
 
 
