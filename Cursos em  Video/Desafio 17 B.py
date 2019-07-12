27bc7b2e53cef433c17103df4421bfe30218b086
tnegraoo <thiagoorbite@gmail.com>
2019-07-12 12:16:06 -0300

    Conversor de temperaturas 1.2 Version B

--- Cursos em  Video/Desafio 17 B.py
+++ Cursos em  Video/Desafio 17 B.py
@@ -1,5 +1,7 @@
-import math
+#import math
+from math import hypot
 co = float(input('Comprimento do cateto oposto: '))
 ca = float(input('Comprimento do cateto adjacente: '))
-hi = math.hypot(co, ca)
+#hi = math.hypot(co, ca)
+hi = hypot(co,ca)
 print('A hipotenusa vai medir {:.2f}'.format(hi))
