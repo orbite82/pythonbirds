d6a38025d342513d3e527cdc7baedee1ab0dbee2
tnegraoo <thiagoorbite@gmail.com>
2019-07-12 15:40:47 -0300

    Conversor de temperaturas 1.0 B

--- Cursos em  Video/Desafio 18 B.py
+++ Cursos em  Video/Desafio 18 B.py
@@ -1,8 +1,8 @@
-import math
+from math import radians, sin, cos, tan
 angulo = float(input('Digite o ângulo que você deseja: '))
-seno = math.sin(math.radians(angulo))
+seno = sin(radians(angulo))
 print('O ângulo de {} tem o SENO de {:.2f}'.format(angulo, seno))
-cosseno = math.cos(math.radians(angulo))
+cosseno = cos(radians(angulo))
 print('O ângulo de {} tem o COSSENO de {:.2f}'.format(angulo, cosseno))
-tangente = math.tan(math.radians(angulo))
+tangente = tan(radians(angulo))
 print('O ângulo de {} tem a TANGENTE de {:.2f}'.format(angulo, tangente))
