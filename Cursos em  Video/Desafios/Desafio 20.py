da623bffd77c2a553a1ea63ab71ceeb54ad8b3c7
tnegraoo <thiagoorbite@gmail.com>
2019-07-12 16:07:42 -0300

    Sorteando uma ordem na lista 1.2

--- Cursos em  Video/Desafios/Desafio 20.py
+++ Cursos em  Video/Desafios/Desafio 20.py
@@ -1,9 +1,11 @@
-import random
+from random import shuffle
+#import random
 n1 = str(input(' 1 Aluno: '))
 n2 = str(input(' 2 Aluno: '))
 n3 = str(input(' 3 Aluno: '))
 n4 = str(input(' 4 Aluno: '))
 lista = [n1, n2, n3, n4]
-random.shuffle(lista)
+#random.shuffle(lista)
+shuffle(lista)
 print(' A ordem de apresentação será: ')
 print(lista)
