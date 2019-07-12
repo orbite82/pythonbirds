87c3191d2c19b8194b83a28c3e3469536246edd9
tnegraoo <thiagoorbite@gmail.com>
2019-07-12 12:25:05 -0300

    Conversor de temperaturas 1.2

--- Cursos em  Video/Desafio 18.py
+++ Cursos em  Video/Desafio 18.py
@@ -1,7 +1,7 @@
 import math
 angulo = float(input('Digite o ângulo que você deseja: '))
 seno = math.sin(math.radians(angulo))
-print('O ângulo de {} tem o SENO de {}'.format(angulo, seno))
+print('O ângulo de {} tem o SENO de {:.2f}'.format(angulo, seno))
 
 
 
