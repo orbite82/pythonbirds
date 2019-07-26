707196f453f7d5676145875e476bb8956e527fa0
tnegraoo <thiagoorbite@gmail.com>
2019-07-26 11:20:25 -0300

    Condição 1.1

--- Cursos em  Video/Aulas/Aula10.py
+++ Cursos em  Video/Aulas/Aula10.py
@@ -1,4 +1,6 @@
 nome = str(input('Qual é seu nome? '))
 if nome == 'Orbite':
     print('Que nome FoDA você tem!')
+else:
+    print('Bom dia, seu nome é normal ok')
 print('Bom dia {}'.format(nome))
